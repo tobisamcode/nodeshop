491546c9622865ccf0e9a529c336fc94b57ceb87
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-25 23:24:23 +0100

    mongoDB

--- util/database.js
+++ util/database.js
@@ -1,1 +1,29 @@
 const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    "mongodb+srv://nodeshop:nodeshop1234@nodeninja.6cx5y.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then(client => {
+      console.log("connected");
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
