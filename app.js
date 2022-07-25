776b9156aeb5483adf958e1940cdc225f14596a8
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-25 23:24:16 +0100

    mongoDB

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -25,7 +24,12 @@ app.use((req, res, next) => {
   //   .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+mongoConnect(client => {
+  console.log(client);
+  app.listen(3000);
+});
