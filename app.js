3fe1b1bbbaec7e0cb5ee3647bf19dc4201cd85b9
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-25 23:38:49 +0100

    mongoDB

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
