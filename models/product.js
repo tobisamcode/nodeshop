01c8ab6093e1c83fd73296bd94f6179546dcba08
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-26 01:28:34 +0100

    mongoDB

--- models/product.js
+++ models/product.js
@@ -21,6 +21,21 @@ class Product {
         console.log(err);
       });
   }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 }
 
 module.exports = Product;
