757a307b256c95d7b9d7605be42f9823e8bda316
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-26 02:07:24 +0100

    mongoDB

--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
-// router.get("/products/:productId", shopController.getProduct);
+router.get("/products/:productId", shopController.getProduct);
 
 // router.get("/cart", shopController.getCart);
 
