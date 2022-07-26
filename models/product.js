a5a07f193e5e3594e1c8e1fac0beee4a802eb4e9
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-26 02:07:16 +0100

    mongoDB

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -10,7 +11,6 @@ class Product {
 
   save() {
     const db = getDb();
-
     return db
       .collection("products")
       .insertOne(this)
@@ -31,6 +31,21 @@ class Product {
       .then(products => {
         console.log(products);
         return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
       })
       .catch(err => {
         console.log(err);
