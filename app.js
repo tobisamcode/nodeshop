99d9a753e9bfe1e3fc570736b914f14469a678c1
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-27 02:32:00 +0100

    mongoDB

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("62e08140827e482b1c4c797c")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
