35411bb8825d2dd50b9dd37bd0c24e5d58b18bfc
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-27 18:44:20 +0100

    mongoDB

--- models/user.js
+++ models/user.js
@@ -70,6 +70,19 @@ class User {
       });
   }
 
+  deleteCart(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCartItems } }
+      );
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
