62d2cafeec3bf26bb361da204e145b6bf1a2b5e5
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-27 01:14:34 +0100

    mongoDB

--- models/product.js
+++ models/product.js
@@ -1,13 +1,15 @@
 const mongodb = require("mongodb");
+const { use } = require("../routes/admin");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = new mongodb.ObjectId(userId);
   }
 
   save() {
