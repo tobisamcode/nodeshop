19eae3c7f7370b9b56aaed30a2623b0c064bef17
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-27 18:44:25 +0100

    mongoDB

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const path = require("path");
 const express = require("express");
 
 const shopController = require("../controllers/shop");
-const { route } = require("./admin");
 
 const router = express.Router();
 
@@ -17,7 +16,7 @@ router.get("/cart", shopController.getCart);
 
 router.post("/cart", shopController.postCart);
 
-// router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 
 // router.post("/create-order", shopController.postOrder);
 
