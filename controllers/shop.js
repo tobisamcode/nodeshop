ab4c6d82955e2d02758264674680664d24d0e09e
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-27 18:44:13 +0100

    mongoDB

--- controllers/shop.js
+++ controllers/shop.js
@@ -66,15 +66,8 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
-    .then(result => {
+    .deleteCart(prodId)
+    .then(() => {
       res.redirect("/cart");
     })
     .catch(err => console.log(err));
