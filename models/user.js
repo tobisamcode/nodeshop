d986f834a9cd75b717283899698d5d9e945f7bfa
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-28 05:31:44 +0100

    mongoose-mongoDB

--- models/user.js
+++ models/user.js
@@ -1,3 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          required: true
+        },
+        quantity: {
+          type: Number,
+          required: true
+        }
+      }
+    ]
+  }
+});
+
 // const getDb = require("../util/database").getDb;
 
 // const mongodb = require("mongodb");
@@ -98,4 +127,4 @@
 //   }
 // }
 
-// module.exports = User;
+module.exports = mongoose.model("User", userSchema);
