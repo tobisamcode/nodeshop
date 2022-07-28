8211ba7bbe17437111723c6801d55608765e6ee8
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-28 05:21:01 +0100

    mongoose-mongoDB

--- controllers/admin.js
+++ controllers/admin.js
@@ -80,12 +80,12 @@ exports.getProducts = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   Product.deleteById(prodId)
-//     .then(() => {
-//       console.log("DESTROYED PRODUCT");
-//       res.redirect("/admin/products");
-//     })
-//     .catch(err => console.log(err));
-// };
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findByIdAndRemove(prodId)
+    .then(() => {
+      console.log("DESTROYED PRODUCT");
+      res.redirect("/admin/products");
+    })
+    .catch(err => console.log(err));
+};
