247d83a09146d12497659f49b489adf906e65250
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-28 12:47:16 +0100

    mongoose-mongoDB

--- models/user.js
+++ models/user.js
@@ -28,27 +28,29 @@ const userSchema = new Schema({
   }
 });
 
-// const getDb = require("../util/database").getDb;
+userSchema.methods.addToCart = function(product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
 
-// const mongodb = require("mongodb");
-// const ObjectId = mongodb.ObjectId;
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCart;
 
-// class User {
-//   constructor(username, email, cart, _id) {
-//     this.name = username;
-//     this.email = email;
-//     this.cart = cart; // {items: []}
-//     this._id = _id;
-//   }
-
-//   save() {
-//     const db = getDb();
-//     return db
-//       .collection("users")
-//       .insertOne(this)
-//       .then(result => console.log(result))
-//       .catch(err => console.log(err));
-//   }
+  return this.save();
+};
 
 //   addToCart(product) {
 //     const cartProductIndex = this.cart.items.findIndex(cp => {
