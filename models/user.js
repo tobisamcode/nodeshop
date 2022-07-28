1a99a77b367f1d64ee96a7a7e8798360b114414d
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-28 16:57:51 +0100

    mongoose-mongoDB

--- models/user.js
+++ models/user.js
@@ -52,6 +52,14 @@ userSchema.methods.addToCart = function(product) {
   return this.save();
 };
 
+userSchema.methods.deleteCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
 //   addToCart(product) {
 //     const cartProductIndex = this.cart.items.findIndex(cp => {
 //       return cp.productId.toString() === product._id.toString();
