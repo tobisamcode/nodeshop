21b160b1157d6233eb60889ac62c7e293f57b45c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-28 18:52:23 +0100

    mongoose-mongoDB

--- controllers/shop.js
+++ controllers/shop.js
@@ -81,7 +81,7 @@ exports.postOrder = (req, res, next) => {
     .populate("cart.items.productId")
     .then(user => {
       const products = user.cart.items.map(i => {
-        return { quantity: i.quantity, product: i.productId };
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
       });
       const order = new Order({
         user: {
